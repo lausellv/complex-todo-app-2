29283ccb546fa5eba2cdbde24347686a2cc46f54
Victor Lausell <lausellv@yahoo.com>
2021-05-10 15:11:48 +0200

    using environment variables

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
-const mongodb = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+mongodb = require('mongodb');
 
 
-const connectionString =
-'mongodb+srv://todoAppUser:pobox00603@cluster0.hgrns.mongodb.net/ComplexTodoApp?retryWrites=true&w=majority';
+
 
 //mongodb.connect(a=connectionString,{},c=function (err, client))
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client){
+  // firts we access mongodb
 module.exports = client.db();  // from here we can find the collection
+//now we can access the app
 const app = require('./app');
-app.listen(3000, "127.0.0.1", function (){
-    console.log('Listening on port 3000')
+app.listen(process.env.PORT, "127.0.0.1", function (){
+    console.log(`Listening on ${process.env.PORT}`)
   })
 
 })
