c10b8e10d04014f6e653932c757a99eedb0baf21
Victor Lausell <lausellv@yahoo.com>
2021-05-10 14:49:50 +0200

    added comments

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-
+//requiring mongodb users collection
 const usersCollection = require('../db').collection('users');
-
+//email "validator" check package 
 const validator = require('validator');
 // Step 1 - user properties
 //setting up a constructor function to define our users 
@@ -9,20 +9,25 @@ let User = function (data) {
   this.data = data; /*this.pizza*/
   this.errors = [];
 };
-// Step 2 - user methods
+// Step 2 - User methods
 // adding a method to our constructor function /more efficient bc we don't have to access methods unless a req is made.  The User will have access to them but not all users will need to have access to all the methods (ie - potential situations that may arise)
+
+
+
+// cleanup function will revert the user data property(ies) back to zero if there aren't strings
 User.prototype.cleanUp = function () {
-  if (typeof this.data.username != 'string') {
+  //typeof (a) or typeof a 
+  if (typeof (this.data.username) != 'string') {
     this.data.username = '';
   }
   if (typeof this.data.email != 'string') {
     this.data.email = '';
   }
-  if (typeof this.data.password != 'string') {
-    this.data.email = '';
+  if (typeof (this.data.password) != 'string') {
+    this.data.password = '';
   }
-
   // get rid of any bogus properties by overwriting any possible additional keys or properties
+  //trim() will get rid of empty spaces at the beginning or the end, toLowerCase() obvious 
   this.data = {
     username: this.data.username.trim().toLowerCase(),
     email: this.data.email.trim().toLowerCase(),
@@ -35,6 +40,8 @@ User.prototype.validate = function () {
   if (this.data.username == '') {
     this.errors.push('You must provide a username');
   }
+  //if they type something in and that thing they typed is validator.isAlphanumeric(a) (returns True or false)
+  // here again we use != && !  - if both conditions are TRUE we then ! them to make them false and once again the if(true) won't run the code (won't push any errors)
   if (this.data.username != '' && !validator.isAlphanumeric(this.data.username)) {
     this.errors.push('Username can only contain letters and numbers');
   }
@@ -45,6 +52,7 @@ User.prototype.validate = function () {
     this.errors.push('Username must be at least 3 characters long');
   }
 
+  //validator.isEmail(a)  returns True or False, once we know it is a valid email (TRUE) we don't need to push an item to the errors array so instead we use ! to do the opposite.  Now , if the user types in a valid email the if (!True) = False so nothing is pushed onto the errors array. 
   if (!validator.isEmail(this.data.email)) {
     this.errors.push('You must provide a valid email');
   }
@@ -63,7 +71,7 @@ User.prototype.validate = function () {
 
 User.prototype.register = function () {
   //step 1 - validate userdata  see User.prototype.cleanup and User.prototype.validate
-  this.cleanUp();
+  this.cleanUp();   // notice we cleanup the data before we send it over for validation
   this.validate();
 
   //step 2 - only if there are no validation errors then save userdata into a database
