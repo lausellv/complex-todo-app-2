f244feb822e0fe7377bf033ef3d02611b23b3dc7
Victor Lausell <lausellv@yahoo.com>
2021-05-10 12:49:09 +0200

    spelling mistake fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const router = require('./router')
 
-app.use(express.urlencoded({extemded:false}));
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static('public'));
 app.set('views', 'views');
