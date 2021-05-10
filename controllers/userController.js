47b45372ca0821f6d996bb14393acd24afe87c28
Victor Lausell <lausellv@yahoo.com>
2021-05-10 15:54:33 +0200

    login func added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
 const User = require('../models/User');
 
-exports.login = function () {};
+exports.login = function (req, res, next) {
+let user = new User(req.body);
+user.login(function (result) {
+  res.send(result);
+  
+});
+
+
+};
 
 exports.logout = function () {};
 
 exports.register = function (req, res) {
-  console.log(req.body)
    let user = new User(req.body);
    user.register();
    // if length is 0 condition won't be met
