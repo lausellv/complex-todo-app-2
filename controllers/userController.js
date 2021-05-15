c7ee988d4f78835b2e895d9f22e472e43c7b5360
Victor <lausellv@yahoo.com>
2021-05-15 23:23:19 +0200

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,8 @@
 const { request } = require('../app');
 const User = require('../models/User');
 
-<<<<<<< HEAD
 
-//user login using Promise
+//Part 1 user login using Promise
 // exports.login = function (req, res, next) {
 // let user = new User(req.body);
 // user.login().then(function (result){
@@ -13,7 +12,7 @@ const User = require('../models/User');
 // });
 // };
 
-
+//Part 1a added session validation 
 exports.login = function (req, res) {
   let user = new User (req.body);
 user.login().then(function(itworks){
@@ -32,25 +31,6 @@ user.login().then(function(itworks){
 // })
 // }
 
-=======
-//using a promise
-exports.login = function (req, res) {
-let user = new User(req.body);
-user.login().then(function (result){
-  res.send(result)
-}).catch(function (err){
-  res.send(err)
-})
- };
-
-// traditional way using a callback function
-// exports.login = function (req, res) {
-//   let user = new User(req.body);
-//   user.login(function (result){  // this is the callback from Users.prototype.login we are passing in as an argument
-// res.send(result)  // this is what we send to the viewer // whatever comes out from the User.prototype.login callback depending on the if or else outcome
-//   })
-// }
->>>>>>> e7ce7d99d19e32ba931a89605cfe9200c4d83df4
 
 exports.logout = function () {};
 
