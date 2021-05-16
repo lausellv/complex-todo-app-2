7d16cfedda982eb83c69f73b42685dce52dcb798
Victor <lausellv@yahoo.com>
2021-05-16 13:43:44 +0200

    added express-session

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-const express = require ('express');
-const session = require ('express-session');
+const express = require('express');
+const session = require('express-session');
 const app = express();
 
-let sessionOptions = session({secret: 'JavaScript is amazing', 
-resave: false,
-saveUninitialized: false,
-cookie: {maxAge: 1000 * 60 *60 * 24, httpOnly: true}
-})
+//leveraging the sessions package
+let sessionOptions = session({
+  secret: 'JavaScript is amazing!!',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },  // represents one day
+});
 
 app.use(sessionOptions);
-const router = require('./router')
+const router = require('./router');
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-app.use('/', router);  // this is our universal entry point for our router
+app.use('/', router); // this is our universal entry point for our router
 
-
-
-
-
-module.exports = app; 
+module.exports = app;
 // app.listen(3000, "127.0.0.1", function (){
 //   console.log('Listening on port 3000')
 // })
