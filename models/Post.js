2760599938f55dde7eeb3b5ed4075a2c5737bc62
Victor <lausellv@yahoo.com>
2021-06-12 15:53:03 +0200

    added the agregate mongodb function

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
-const postsCollection = require ('../db').db().collection("posts");
-const ObjectID = require('mongodb').ObjectID
+const postsCollection = require('../db').db().collection('posts');
+const ObjectID = require('mongodb').ObjectID;
 
 let Post = function (data, userid) {
-  this.data = data
-  this.errors = []
-  this.userid = userid
+  this.data = data;
+  this.errors = [];
+  this.userid = userid;
 };
 Post.prototype.cleanUp = function () {
   if (typeof this.data.title != 'string') {
@@ -14,40 +14,68 @@ Post.prototype.cleanUp = function () {
     this.data.body = '';
   }
 
-  // get rid of any bogus properties 
+  // get rid of any bogus properties
   this.data = {
     title: this.data.title.trim(),
     body: this.data.body.trim(),
     createdDate: new Date(),
-    author: ObjectID(this.userid)  // mongodb prefers it fi we store an ID value ie ObjectId object type
+    author: ObjectID(this.userid), // mongodb prefers it fi we store an ID value ie ObjectId object type
   };
 };
 Post.prototype.validate = function () {
-  if ( this.data.title ==''){
-this.errors.push ('You must provide a title.')
+  if (this.data.title == '') {
+    this.errors.push('You must provide a title.');
   }
-  if ( this.data.body ==''){
-    this.errors.push ('You must provide post content.')
-      }
+  if (this.data.body == '') {
+    this.errors.push('You must provide post content.');
+  }
 };
 
 Post.prototype.create = function () {
-  return new Promise ((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     this.cleanUp();
     this.validate();
-    if(!this.errors.length){
+    if (!this.errors.length) {
       //save post into db
-postsCollection.insertOne(this.data).then(()=>{
-  resolve()
-}).catch(()=>{
-  this.errors.push('Please try again later.')
-  reject(this.errors)
-})
-    
-}else{
-      reject(this.errors)
+      postsCollection
+        .insertOne(this.data)
+        .then(() => {
+          resolve();
+        })
+        .catch(() => {
+          this.errors.push('Please try again later.');
+          reject(this.errors);
+        });
+    } else {
+      reject(this.errors);
     }
-  })
+  });
+};
+
+Post.findSingleById = function (id) {
+  return new Promise(async function (resolve, reject) {
+    if (typeof (id) != 'string' || !ObjectID.isValid(id)) {
+      reject();
+      return;
+    }
+    let posts = await postsCollection.aggregate([
+      {$match: {_id: new ObjectID(id)}},
+      {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
+      {$project: {
+        title: true,  /*you can use 1 or true*/
+        body: 1,
+        createdDate: 1,
+        author: {$arrayElemAt: ["$authorDocument", 0]}
+      }}
+      ])
+      .toArray();
+    if (posts.length) {
+      console.log(posts[0])
+      resolve(posts[0]);
+    } else {
+      reject();
+    }
+  });
 };
 
 module.exports = Post;
