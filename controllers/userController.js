8486ab32282183356873faa6dbb555d9813a52f3
Victor <lausellv@yahoo.com>
2021-06-12 18:50:00 +0200

    added ifUserExists function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 //const { request } = require('../app');
-const User = require('../models/User');
+const User = require("../models/User");
 
 //Part 1 user login using Promise
 // exports.login = function (req, res, next) {
@@ -10,68 +10,95 @@ const User = require('../models/User');
 //   res.send(e)
 // });
 // };
-exports.mustBeLoggedIn = function (req, res, next){
-  if (req.session.user){
-next()
-  }else{
-req.flash('errors', 'You must be logged in to perform that action.')
-req.session.save(function (){
-  res.redirect('/')
-})
+exports.mustBeLoggedIn = function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash("errors", "You must be logged in to perform that action.");
+    req.session.save(function () {
+      res.redirect("/");
+    });
   }
-}
+};
 
 //Part 1a added session validation
 exports.login = function (req, res) {
   let user = new User(req.body);
-  user.login().then(function (itworks) {
-      req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id};
+  user
+    .login()
+    .then(function (itworks) {
+      req.session.user = {
+        avatar: user.avatar,
+        username: user.data.username,
+        _id: user.data._id,
+      };
       req.session.save(function () {
-        res.redirect('/');
+        res.redirect("/");
       });
     })
     .catch(function (err) {
       // typically one uses e
-      req.flash('errors', err);
+      req.flash("errors", err);
       // req.session.flash.errors = [doesntWorkError]
       req.session.save(function () {
-        res.redirect('/');
+        res.redirect("/");
       });
     });
 };
 
-
-
 exports.logout = function (req, res) {
   req.session.destroy(function () {
-    res.redirect('/');
+    res.redirect("/");
   });
 };
 
-
 exports.register = async (req, res) => {
   let user = new User(req.body);
-  user.register().then(() => {
-    req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
-    req.session.save(function() {
-      res.redirect('/')
+  user
+    .register()
+    .then(() => {
+      req.session.user = {
+        avatar: user.avatar,
+        username: user.data.username,
+        _id: user.data._id,
+      };
+      req.session.save(function () {
+        res.redirect("/");
+      });
     })
-  }).catch((regErrors) => {
-    regErrors.forEach(function(error) {
-      req.flash('regErrors', error)
-    })
-    req.session.save(function() {
-      res.redirect('/')
-    })
-  })
-}
-
+    .catch((regErrors) => {
+      regErrors.forEach(function (error) {
+        req.flash("regErrors", error);
+      });
+      req.session.save(function () {
+        res.redirect("/");
+      });
+    });
+};
 
 exports.home = function (req, res) {
   if (req.session.user) {
-    res.render('home-dashboard');
+    res.render("home-dashboard");
     /*[0].toUpperCase()+req.session.user.username.toLowerCase().slice(1)*/
   } else {
-    res.render('home-guest', { errors: req.flash('errors'), regErrors: req.flash('regErrors')});
+    res.render("home-guest", {
+      errors: req.flash("errors"),
+      regErrors: req.flash("regErrors"),
+    });
   }
+};
+
+exports.ifUserExists = function (req, res, next) {
+  User.findByUsername(req.params.username)
+    .then(function (userDocument) {
+      req.profileUser = userDocument;
+      next();
+    })
+    .catch(function () {
+      res.render("404");
+    });
+};
+
+exports.profilePostsScreen = function (req, res) {
+  res.render("profile");
 };
